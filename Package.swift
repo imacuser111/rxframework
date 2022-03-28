a9ec6bb8e187b33c628231a516538a610f5c8ece
Cheng-Hong <s26803018@gmail.com>
2022-03-28 11:40:10 +0800

    test

--- Package.swift
+++ Package.swift
@@ -10,7 +10,6 @@ let package = Package(
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(name: "RxWebSocketPackage", targets: ["RxWebSocketPackage"]),
         .library(name: "RxCoreDataPackage", targets: ["RxCoreDataPackage"]),
     ],
     dependencies: [
@@ -22,7 +21,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(name: "RxWebSocketPackage"),
         .target(name: "RxCoreDataPackage"),
     ]
 )
